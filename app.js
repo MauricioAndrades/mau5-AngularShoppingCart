f6b6954e6c467d0021bee67b61009c8ccf05c303
MauricioAndrades <mod.and.op@gmail.com>
2016-02-09 05:13:12 -0800

    octal literals not allowed in strict mode

--- app.js
+++ app.js
@@ -2,19 +2,31 @@
 
 var app = angular.module('modApp', []);
 
-app.controller('PageController', function($scope){
-	$scope.pagetitle = 'The Page Title';
+app.controller('ExerciseController', function($scope) {
+    $scope.FavColor = 'yellow';
+
+    $scope.secondsInACentury = function($scope) {
+    	var startofcentury = new Date(1900,01,01);
+    	var endofcentury = new Date(1999,12,31);
+		var milliSeconds = endofcentury - startofcentury;
+		var seconds = milliSeconds/1000;
+        $scope.seconds = seconds;
+    };
 });
 
-app.controller('FilterController', function($scope){
-	$scope.name = 'john smith';
+app.controller('MyFirstController', function($scope) {
+    $scope.name = 'severus snape';
 });
 
-app.controller('HelloController', function($scope){
-	$scope.hello = {};
-	$scope.hello.title = 'World';
+app.controller('PageController', function($scope) {
+    $scope.pagetitle = 'The Page Title';
 });
 
+app.controller('FilterController', function($scope) {
+    $scope.name = 'john smith';
+});
 
-
-
+app.controller('HelloController', function($scope) {
+    $scope.hello = {};
+    $scope.hello.title = 'World';
+});
